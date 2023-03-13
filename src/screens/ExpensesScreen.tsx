9bea1f67e0a3193cc7595add4af22a1c4179fef2
YU-HONG HU <huyuhong0205@gmail.com>
2023-03-13 21:52:11 +0800

    feat: query expenses in realm

--- src/screens/ExpensesScreen.tsx
+++ src/screens/ExpensesScreen.tsx
@@ -1,14 +1,14 @@
 /* React */
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
 /* Navigation */
 import { CompositeScreenProps, useIsFocused } from '@react-navigation/native';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { StackScreenProps } from '@react-navigation/stack';
 /* Native base */
-import { VStack, FlatList, Fab, Icon } from 'native-base';
+import { VStack, FlatList, Box, Text, Fab, Icon } from 'native-base';
 
 /* DB */
-import { useQuery } from '../models/realm';
+import { useQuery, useRealm } from '../models/realm';
 import Category from '../models/categorySchema';
 import Expense from '../models/expenseSchema';
 /* Components */
@@ -27,8 +27,13 @@ type Props = CompositeScreenProps<
 export default function ExpensesScreen({ navigation }: Props) {
   const isFocused = useIsFocused();
 
+  const realm = useRealm();
   const categoriesInRealm = useQuery(Category);
-  const expensesInRealm = useQuery(Expense);
+
+  // [TODO] if no picked date set date to null query entire month
+  const [pickedDate, setPickedDate] = useState<Date>(new Date('2023-03-12'));
+  const [expenses, setExpenses] =
+    useState<Realm.Results<Expense & Realm.Object<unknown, never>>>();
 
   const categories = useMemo(() => {
     const categoriesMap: Record<
@@ -45,6 +50,16 @@ export default function ExpensesScreen({ navigation }: Props) {
 
     return categoriesMap;
   }, [categoriesInRealm]);
+
+  useEffect(() => {
+    const endDate = new Date(pickedDate.getTime() + 24 * 60 * 60 * 1000 - 1);
+
+    const queryExpenses = realm
+      .objects(Expense)
+      .filtered('date >= $0 && date < $1', pickedDate, endDate);
+
+    setExpenses(queryExpenses);
+  }, [realm, pickedDate]);
 
   /* Event handler ------------------------------------------------ */
   const handleGoExpenseFormScreen = useCallback(() => {
@@ -63,24 +78,32 @@ export default function ExpensesScreen({ navigation }: Props) {
         _light={{ bgColor: 'bgLightMode' }}
         _dark={{ bgColor: 'bgDarkMode' }}
       >
-        <FlatList
-          flex={1}
-          width="full"
-          padding={2}
-          data={expensesInRealm}
-          keyExtractor={(expense) => String(expense._id)}
-          renderItem={({ item: expense }) => (
-            <ExpenseItem
-              _id={String(expense._id)}
-              title={expense.title}
-              amount={expense.amount}
-              isExpense={expense.isExpense}
-              date={expense.date}
-              // categoryName={categories[String(expense.categoryId)].categoryName}
-              iconName={categories[String(expense.categoryId)].iconName}
-            />
-          )}
-        />
+        {expenses && expenses.length > 0 ? (
+          <FlatList
+            flex={1}
+            width="full"
+            padding={2}
+            data={expenses}
+            keyExtractor={(expense) => String(expense._id)}
+            renderItem={({ item: expense }) => (
+              <ExpenseItem
+                _id={String(expense._id)}
+                title={expense.title}
+                amount={expense.amount}
+                isExpense={expense.isExpense}
+                date={expense.date}
+                // categoryName={categories[String(expense.categoryId)].categoryName}
+                iconName={categories[String(expense.categoryId)].iconName}
+              />
+            )}
+          />
+        ) : (
+          <Box flex={1} width="full">
+            <Text padding={4} fontSize="lg" textAlign="center">
+              No expenses found.
+            </Text>
+          </Box>
+        )}
 
         {isFocused && (
           <Fab
